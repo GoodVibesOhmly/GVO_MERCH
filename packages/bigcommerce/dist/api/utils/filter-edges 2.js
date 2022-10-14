ed4656a3bfce09cdb099a236a63a05191c41b9f6
Matthew Mulford <mhmulford0@gmail.com>
2022-10-14 12:35:24 -0500

    Revert "add packages"
    This reverts commit cad4768a64c9ad26a61034ee2b485e1b77ea50b6.

--- packages/bigcommerce/dist/api/utils/filter-edges 2.js
+++
@@ -1,3 +0,0 @@
-export default function filterEdges(edges) {
-    return (edges == null ? void 0 : edges.filter((edge)=>!!edge)) ?? [];
-};
--- packages/commerce/dist/utils/define-property 2.js
+++
@@ -1,4 +0,0 @@
-// Taken from https://fettblog.eu/typescript-assertion-signatures/
-export default function defineProperty(obj, prop, val) {
-    Object.defineProperty(obj, prop, val);
-};
